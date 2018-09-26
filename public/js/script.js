a3dd66f63b73cb8bcf566fd451723e7a0923f2c0
Stephen Latham <slatham@Stephens-iMac.local>
2018-09-26 19:20:12 +0100

    Added the functionality to the toggleDrum function

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,32 @@ let rideCymbals = [false,false,false,false,false,false,false,false,false,false,f
 
 // function definitions
 const toggleDrum = (drumArray, indexNumber) => {
+debugger;
+
+// check for any missing and invalid arguments
+if (!drumArray || indexNumber >= drumArray.length || indexNumber < 0){
+	return // if any are missing or strange ... time to bail!
+}
+
+
+switch (drumArray){
+
+
+	case 'kicks':
+		kicks[indexNumber] = !kicks[indexNumber];
+		break;
+	case 'snares':
+		snares[indexNumber] = !snares[indexNumber];
+		break;
+	case 'hiHats':
+		hiHats[indexNumber] = !hiHats[indexNumber];
+		break;
+	case 'rideCymbals':
+		rideCymbals[indexNumber] = !rideCymbals[indexNumber];
+		break;
+
+}
+
 
 }
 
