d3f60d659f0339af6842c56cd72d6e1ba207d802
Stephen Latham <ste.latham@gmail.com>
2018-09-26 12:06:30 +0100

    Defined the functions

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,19 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
+let snares = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
+let hiHats = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
+let rideCymbals = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
 
+// function definitions
+const toggleDrum = (drumArray, indexNumber) => {
+
+}
+
+const clear = drumArray => {
+
+}
+
+const invert = drumArray => {
+
+  
+}
