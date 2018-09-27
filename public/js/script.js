3ff17965e5c968598221d7bccd5a485691fd1a68
Stephen Latham <ste.latham@gmail.com>
2018-09-27 12:17:10 +0100

    Added the functionality to clear

--- public/js/script.js
+++ public/js/script.js
@@ -4,19 +4,19 @@ let snares = [false,false,false,false,false,false,false,false,false,false,false,
 let hiHats = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
 let rideCymbals = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
 
-// function definitions
+// used to make validation easier
+const validDrumArray = ['kicks', 'snares','hiHats','rideCymbals'];
+
+// function to toggle a drum on or off
 const toggleDrum = (drumArray, indexNumber) => {
-debugger;
 
 // check for any missing and invalid arguments
-if (!drumArray || indexNumber >= drumArray.length || indexNumber < 0){
+if (!validDrumArray.includes(drumArray) || indexNumber >= drumArray.length || indexNumber < 0){
 	return // if any are missing or strange ... time to bail!
 }
 
-
+// for each type of drum toggle it
 switch (drumArray){
-
-
 	case 'kicks':
 		kicks[indexNumber] = !kicks[indexNumber];
 		break;
@@ -29,17 +29,42 @@ switch (drumArray){
 	case 'rideCymbals':
 		rideCymbals[indexNumber] = !rideCymbals[indexNumber];
 		break;
-
+}
 }
 
-
-}
-
+// function to clear the whole array for a particular drum
 const clear = drumArray => {
-
+	// check for a valid array passed
+	if (!validDrumArray.includes(drumArray)){
+		return
+	}
+	// for each drum array loop through and set each element to false
+	switch (drumArray){
+	case 'kicks':
+		kicks.forEach((element,index) => {
+			kicks[index] = false;
+		});
+		break;
+	case 'snares':
+		snares.forEach((element,index) => {
+			snares[index] = false;
+		});
+		break;
+	case 'hiHats':
+		hiHats.forEach((element,index) => {
+			hiHats[index] = false;
+		});
+		break;
+	case 'rideCymbals':
+		rideCymbals.forEach((element,index) => {
+			rideCymbals[index] = false;
+		});
+		break;
+}
 }
 
+// function to invert a whole drum array
 const invert = drumArray => {
 
-  
+
 }
